de906c20a58bcb14f5b40f1aaa88024a2b65d318
JuanManuelGarciaUTN <artlixUTN@gmail.com>
2022-03-24 14:23:30 -0300

    Crear Repositorio Ejercicios

---
+++ Soluciones/Clase1 - Ejercicio1/Clase1 - Ejercicio1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Clase1___Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
