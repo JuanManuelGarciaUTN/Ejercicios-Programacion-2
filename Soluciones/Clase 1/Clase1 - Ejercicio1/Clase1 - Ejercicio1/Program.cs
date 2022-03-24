dc2f0e44119464588fe82202773538408445a000
JuanManuelGarciaUTN <artlixUTN@gmail.com>
2022-03-24 14:51:15 -0300

    Hasta ejercicio 2 Clase 1

---
+++ Soluciones/Clase 1/Clase1 - Ejercicio1/Clase1 - Ejercicio1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Clase1___Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float numeroIngresado;
+            float numeroMaximo;
+            float numeroMinimo;
+            float promedio;
+
+            const int cantidadNumeros = 5;
+
+            promedio = 0;
+            numeroMaximo = float.MinValue;
+            numeroMinimo = float.MaxValue;
+
+            for (int i = 0; i < cantidadNumeros; i++)
+            {
+                Console.Write("Ingrese un numero: ");
+                numeroIngresado = float.Parse(Console.ReadLine());
+
+                if(numeroIngresado < numeroMinimo )
+                {
+                    numeroMinimo = numeroIngresado;
+                }
+                if (numeroIngresado > numeroMaximo)
+                {
+                    numeroMaximo = numeroIngresado;
+                }
+
+                promedio += numeroIngresado;
+            }
+
+            promedio = promedio / cantidadNumeros;
+
+            Console.WriteLine("\nEl numero maximo ingresado es: {0}", numeroMaximo);
+            Console.WriteLine("El numero minimo ingresado es: {0}", numeroMinimo);
+            Console.WriteLine("El promedio de los numeros ingresados es: {0}", promedio);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Soluciones/Clase 1/Clase1 - Ejercicio2/Clase1 - Ejercicio2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Clase1___Ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float numeroIngresado = 0;
+            float potencia;
+            bool valorInvalido = true;
+
+            while (valorInvalido)
+            {
+                Console.Write("Ingrese un numero positivo a calcular potencia: ");
+                numeroIngresado = float.Parse(Console.ReadLine());
+
+                if(numeroIngresado > 0)
+                {
+                    valorInvalido = false;
+                }
+                else
+                {
+                    Console.WriteLine("\nERROR ¡Reingresar número!\n");
+                }
+            }
+
+            potencia = (float)Math.Pow(numeroIngresado, 2);
+            Console.WriteLine("{0} elevado al cuadrado es igual a: {1}", numeroIngresado, potencia);
+
+            potencia = (float)Math.Pow(numeroIngresado, 3);
+            Console.WriteLine("{0} elevado al cubo es igual a: {1}", numeroIngresado, potencia);
+
+            Console.ReadKey();
+        }
+    }
+}
